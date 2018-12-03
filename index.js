182420fb63be2b1c14210b682120b1f8d376befd
bevchou <31548243+bevchou@users.noreply.github.com>
2018-12-03 16:10:13 -0500

    altered for my server

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 const Joi = require('joi');
 const express = require('express');
+const http = require('http');
 const app = express();
 
 app.use(express.json());
 
 const songs = [
-    {id: 1, title: 'I’m A Believer', artist: 'The Monkees', mood: 'happy', genre: 'rock', url : 'http://206.189.186.47:3000/sound_files/1.wav'},
-    {id: 2, title: 'Beautiful Day', artist: 'U2', mood: 'happy', genre: 'pop', url : 'http://206.189.186.47:3000/sound_files/2.wav' },
-    {id: 3, title: 'No Problem', artist: 'Chance The Rapper', mood: 'happy', genre: 'hip-hop', url : 'http://206.189.186.47:3000/sound_files/3.wav' },
-    {id: 4, title: 'Freddie Freeloader', artist: 'Miles Davis', mood: 'happy', genre: 'jazz', url : 'http://206.189.186.47:3000/sound_files/4.wav' },
-    {id: 5, title: 'Sonata No. 17 in C', artist: 'Mozart', mood: 'happy', genre: 'classical', url : 'http://206.189.186.47:3000/sound_files/5.wav' },
-    {id: 6, title: 'Middle of the Road', artist: 'The Pretenders', mood: 'sad', genre: 'rock', url : 'http://206.189.186.47:3000/sound_files/6.wav' },
-    {id: 7, title: 'Someone Like You', artist: 'Adele', mood: 'sad', genre: 'pop', url : 'http://206.189.186.47:3000/sound_files/7.wav' },
-    {id: 8, title: 'Stan', artist: 'Eminem', mood: 'sad', genre: 'hip-hop', url : 'http://206.189.186.47:3000/sound_files/8.wav' },
-    {id: 9, title: 'How High The Moon', artist: 'Ella Fitzgerald', mood: 'sad', genre: 'jazz', url : 'http://206.189.186.47:3000/sound_files/9.wav' },
-    {id: 10, title: 'Nocturnes', artist: 'Frédéric Chopin', mood: 'sad', genre: 'classical', url : 'http://206.189.186.47:3000/sound_files/10.wav' },
-    {id: 11, title: 'St. Anger', artist: 'Metallica', mood: 'angry', genre: 'rock', url : 'http://206.189.186.47:3000/sound_files/11.wav' },
-    {id: 12, title: 'Sail', artist: 'AWOLNATION', mood: 'angry', genre: 'pop', url : 'http://206.189.186.47:3000/sound_files/12.wav' },
-    {id: 13, title: 'Black Skinhead', artist: 'Kanye West', mood: 'angry', genre: 'hip-hop', url : 'http://206.189.186.47:3000/sound_files/13.wav' },
-    {id: 14, title: 'Africa', artist: 'John Coltrane', mood: 'angry', genre: 'jazz', url : 'http://206.189.186.47:3000/sound_files/14.wav' },
-    {id: 15, title: '5th Symphony', artist: 'Beethoven', mood: 'angry', genre: 'classical', url : 'http://206.189.186.47:3000/sound_files/15.wav' },
-    {id: 16, title: 'Black Sabbath', artist: 'Black Sabbath', mood: 'scared', genre: 'rock', url : 'http://206.189.186.47:3000/sound_files/16.wav' },
-    {id: 17, title: 'Haunted', artist: 'Beyonce', mood: 'scared', genre: 'pop', url : 'http://206.189.186.47:3000/sound_files/17.wav' },
-    {id: 18, title: 'Kids See Ghosts', artist: 'Kids See Ghosts', mood: 'scared', genre: 'hip-hop', url : 'http://206.189.186.47:3000/sound_files/18.wav' },
-    {id: 19, title: 'The Valley of the Shadows', artist: 'Bob James', mood: 'scared', genre: 'jazz', url : 'http://206.189.186.47:3000/sound_files/19.wav' },
-    {id: 20, title: 'In The Hall Of The Mountain King', artist: 'Edvard Grieg', mood: 'scared', genre: 'classical', url : 'http://206.189.186.47:3000/sound_files/20.wav' },
-    {id: 21, title: 'Comfortably Numb', artist: 'Pink Floyd', mood: 'chill', genre: 'rock', url : 'http://206.189.186.47:3000/sound_files/21.wav' },
-    {id: 22, title: 'Perfect To Me', artist: 'Anne-Marie', mood: 'chill', genre: 'pop', url : 'http://206.189.186.47:3000/sound_files/22.wav' },
-    {id: 23, title: 'Self Care', artist: 'Mac Miller', mood: 'chill', genre: 'hip-hop', url : 'http://206.189.186.47:3000/sound_files/23.wav' },
-    {id: 24, title: 'Sophisticated Lady', artist: 'Sarah Vaughan', mood: 'chill', genre: 'jazz', url : 'http://206.189.186.47:3000/sound_files/24.wav' },
-    {id: 25, title: 'Gymnopedie No.1', artist: 'Erik Satie', mood: 'chill', genre: 'classical', url : 'http://206.189.186.47:3000/sound_files/25.wav' },
+    {id: 1, title: 'I’m A Believer', artist: 'The Monkees', mood: 'happy', genre: 'rock', url : 'bc2542.itp.io/sound_files/1.wav'},
+    {id: 2, title: 'Beautiful Day', artist: 'U2', mood: 'happy', genre: 'pop', url : 'bc2542.itp.io/sound_files/2.wav' },
+    {id: 3, title: 'No Problem', artist: 'Chance The Rapper', mood: 'happy', genre: 'hip-hop', url : 'bc2542.itp.io/sound_files/3.wav' },
+    {id: 4, title: 'Freddie Freeloader', artist: 'Miles Davis', mood: 'happy', genre: 'jazz', url : 'bc2542.itp.io/sound_files/4.wav' },
+    {id: 5, title: 'Sonata No. 17 in C', artist: 'Mozart', mood: 'happy', genre: 'classical', url : 'bc2542.itp.io/sound_files/5.wav' },
+    {id: 6, title: 'Middle of the Road', artist: 'The Pretenders', mood: 'sad', genre: 'rock', url : 'bc2542.itp.io/sound_files/6.wav' },
+    {id: 7, title: 'Someone Like You', artist: 'Adele', mood: 'sad', genre: 'pop', url : 'bc2542.itp.io/sound_files/7.wav' },
+    {id: 8, title: 'Stan', artist: 'Eminem', mood: 'sad', genre: 'hip-hop', url : 'bc2542.itp.io/sound_files/8.wav' },
+    {id: 9, title: 'How High The Moon', artist: 'Ella Fitzgerald', mood: 'sad', genre: 'jazz', url : 'bc2542.itp.io/sound_files/9.wav' },
+    {id: 10, title: 'Nocturnes', artist: 'Frédéric Chopin', mood: 'sad', genre: 'classical', url : 'bc2542.itp.io/sound_files/10.wav' },
+    {id: 11, title: 'St. Anger', artist: 'Metallica', mood: 'angry', genre: 'rock', url : 'bc2542.itp.io/sound_files/11.wav' },
+    {id: 12, title: 'Sail', artist: 'AWOLNATION', mood: 'angry', genre: 'pop', url : 'bc2542.itp.io/sound_files/12.wav' },
+    {id: 13, title: 'Black Skinhead', artist: 'Kanye West', mood: 'angry', genre: 'hip-hop', url : 'bc2542.itp.io/sound_files/13.wav' },
+    {id: 14, title: 'Africa', artist: 'John Coltrane', mood: 'angry', genre: 'jazz', url : 'bc2542.itp.io/sound_files/14.wav' },
+    {id: 15, title: '5th Symphony', artist: 'Beethoven', mood: 'angry', genre: 'classical', url : 'bc2542.itp.io/sound_files/15.wav' },
+    {id: 16, title: 'Black Sabbath', artist: 'Black Sabbath', mood: 'scared', genre: 'rock', url : 'bc2542.itp.io/sound_files/16.wav' },
+    {id: 17, title: 'Haunted', artist: 'Beyonce', mood: 'scared', genre: 'pop', url : 'bc2542.itp.io/sound_files/17.wav' },
+    {id: 18, title: 'Kids See Ghosts', artist: 'Kids See Ghosts', mood: 'scared', genre: 'hip-hop', url : 'bc2542.itp.io/sound_files/18.wav' },
+    {id: 19, title: 'The Valley of the Shadows', artist: 'Bob James', mood: 'scared', genre: 'jazz', url : 'bc2542.itp.io/sound_files/19.wav' },
+    {id: 20, title: 'In The Hall Of The Mountain King', artist: 'Edvard Grieg', mood: 'scared', genre: 'classical', url : 'bc2542.itp.io/sound_files/20.wav' },
+    {id: 21, title: 'Comfortably Numb', artist: 'Pink Floyd', mood: 'chill', genre: 'rock', url : 'bc2542.itp.io/sound_files/21.wav' },
+    {id: 22, title: 'Perfect To Me', artist: 'Anne-Marie', mood: 'chill', genre: 'pop', url : 'bc2542.itp.io/sound_files/22.wav' },
+    {id: 23, title: 'Self Care', artist: 'Mac Miller', mood: 'chill', genre: 'hip-hop', url : 'bc2542.itp.io/sound_files/23.wav' },
+    {id: 24, title: 'Sophisticated Lady', artist: 'Sarah Vaughan', mood: 'chill', genre: 'jazz', url : 'bc2542.itp.io/sound_files/24.wav' },
+    {id: 25, title: 'Gymnopedie No.1', artist: 'Erik Satie', mood: 'chill', genre: 'classical', url : 'bc2542.itp.io/sound_files/25.wav' },
 ];
 
 var state = {
@@ -211,10 +212,10 @@ app.post('/api/playlist', (req, res) => {
 // PUT a song to Player (Play song)
 app.put('/api/player/play', (req, res) => {
 
-    if (state.isPlaying == false) {
-        return res.status(404).send('There is no song to play.');
-    }
-    
+    // if (state.isPlaying == false) {
+    //     return res.status(404).send('There is no song to play.');
+    // }
+
     state = {
         id : req.body.id,
         title: req.body.title,
@@ -224,7 +225,7 @@ app.put('/api/player/play', (req, res) => {
         genre: req.body.genre,
         url: req.body.url
     }
-    
+
     res.send(state);
 });
 
